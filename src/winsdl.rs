7096d164f1aace9fdd041e3a23f807f418f86e0c
Brayan Zavala <zavalabrayan244@gmail.com>
2024-02-15 21:32:32 -0600

    refactors windsdl

--- src/winsdl.rs
+++ src/winsdl.rs
@@ -13,26 +13,14 @@ impl WinSdl {
         let sdl = sdl2::init().unwrap();
         let video_subsystem = sdl.video().unwrap();
 
-        // set OpenGL version
-        let gl_attr = video_subsystem.gl_attr();
-        gl_attr.set_context_profile(sdl2::video::GLProfile::Core);
-        gl_attr.set_context_version(3, 3);
+        Self::set_gl_attributes(&video_subsystem);
 
-        let window = video_subsystem
-            .window("SDL2", width as u32, height as u32)
-            .opengl()
-            .build()
-            .unwrap();
+        let window = Self::create_window(&video_subsystem, width, height);
 
         let gl_context = window.gl_create_context().unwrap();
-        let gl = gl::load_with(|s| {
-            video_subsystem.gl_get_proc_address(s) as *const std::os::raw::c_void
-        });
+        let gl = Self::load_gl(&video_subsystem);
 
-        window
-            .subsystem()
-            .gl_set_swap_interval(sdl2::video::SwapInterval::VSync)
-            .unwrap();
+        Self::set_swap_interval(&window);
 
         let event_pump: sdl2::EventPump = sdl.event_pump().unwrap();
 
@@ -43,5 +31,34 @@ impl WinSdl {
             gl,
             event_pump,
         })
+    }
+
+    fn set_gl_attributes(video_subsystem: &sdl2::VideoSubsystem) {
+        let gl_attr = video_subsystem.gl_attr();
+        gl_attr.set_context_profile(sdl2::video::GLProfile::Core);
+        gl_attr.set_context_version(3, 3);
+    }
+
+    fn create_window(
+        video_subsystem: &sdl2::VideoSubsystem,
+        width: usize,
+        height: usize,
+    ) -> Window {
+        video_subsystem
+            .window("SDL2", width as u32, height as u32)
+            .opengl()
+            .build()
+            .unwrap()
+    }
+
+    fn load_gl(video_subsystem: &sdl2::VideoSubsystem) -> () {
+        gl::load_with(|s| video_subsystem.gl_get_proc_address(s) as *const std::os::raw::c_void)
+    }
+
+    fn set_swap_interval(window: &Window) {
+        window
+            .subsystem()
+            .gl_set_swap_interval(sdl2::video::SwapInterval::VSync)
+            .unwrap();
     }
 }
